6aceee0c3e9c2afa3d8bae610bbe7a921f6e0264
EuWagnera11 <ferramentas@lojaads.com>
2026-04-29 20:41:50 -0300

    Revert "feat(react): migra mockup HTML pra React+Vite mantendo design idêntico"
    This reverts commit 1b1905a6ef78311a842a27b4bb6dc7ca228e642d.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "./styles/mockup.css";
-import RefineApp from "./refine/RefineApp";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<RefineApp />);
+createRoot(document.getElementById("root")!).render(<App />);
